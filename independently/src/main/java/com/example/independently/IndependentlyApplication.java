81ae4911fa7e0d7bf07f555dbb07c05d0836081b
ddohyeong <dohyeong7026@gmail.com>
2024-11-25 22:07:08 +0900

    feat: 하위 모듈 추가

---
+++ independently/src/main/java/com/example/independently/IndependentlyApplication.java
@@ -0,0 +1,13 @@
+package com.example.independently;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndependentlyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IndependentlyApplication.class, args);
+    }
+
+}
---
+++ independently/src/test/java/com/example/independently/IndependentlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.independently;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndependentlyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
